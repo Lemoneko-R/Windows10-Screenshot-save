ca5545d0876908b4b25f180efba012453724c05c
point1reiya <point_yukkuri@outlook.jp>
2019-02-01 23:12:13 +0900

    Add files via upload

---
+++ sc.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from PIL import ImageGrab, Image
+import datetime
+
+def save():
+    im = ImageGrab.grabclipboard()
+    if isinstance(im, Image.Image):
+        im.save(str("D:\\Screenshots\\")+str(datetime.datetime.today()).replace(" ","-").replace(":","-")+'.jpg')
+        print('saved')
+    else:
+        print('no image')
+
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    #この関数が呼ばれているかどうかチェックするための処理
+
+
+    try:
+        if key.char == "Key.ctrl_r":
+            save()
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+
+        it= "{0}".format(
+            key)
+        if it == "Key.ctrl_r":
+            save()
+
+
+if __name__ == '__main__':
+
+    with Listener(
+        on_press = on_press,
+    ) as listener:
+        listener.join()
---
+++ start.vbs
@@ -0,0 +1,3 @@
+Dim WSHShell
+Set WSHShell = WScript.CreateObject("WScript.Shell")
+Call WSHShell.Run("D:\twitter\start.bat", 0, True )
